706ab00b29214ad1d9f59851f5961547ad62a175
Brandon Stevens <brandontylerstevens@gmail.com>
2018-01-21 07:20:14 -0700

    Create FileDialog.ps1

---
+++ Files/FileDialog.ps1
@@ -0,0 +1,15 @@
+#using assembly "System.windows.forms"
+Function GetFileName
+{ 
+param($initialDirectory = "c:\")
+
+#add-type  -AssemblyName "System.windows.forms"
+ $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+ $OpenFileDialog.initialDirectory = $initialDirectory
+ $OpenFileDialog.filter = "All files (*.*)| *.*"
+ $OpenFileDialog.ShowDialog() | Out-Null
+ $OpenFileDialog.filename
+} #end function Get-FileName
+
+GetFileName
+
