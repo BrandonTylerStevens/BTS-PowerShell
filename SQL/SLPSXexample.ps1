13204df0b4879bb71a2cc71f6501c91d123ef163
Brandon Stevens <brandontylerstevens@gmail.com>
2018-01-21 06:54:43 -0700

    Create SLPSXexample.ps1

---
+++ SQL/SLPSXexample.ps1
@@ -0,0 +1,5 @@
+﻿#https://sqlpsx.codeplex.com/
+$SQLDBName = "AdventureWorks"
+$Table = 'HumanResources.Employee'
+$SqlQuery = "select * FROM $Table"
+get-sqldata -sqlserver sql2 -dbname $SQLDBName -qry $SqlQuery
