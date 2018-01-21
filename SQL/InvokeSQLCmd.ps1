a2a44f0bd9b8ef57f97f0ab85e69524d5886c5d6
Brandon Stevens <brandontylerstevens@gmail.com>
2018-01-21 06:53:56 -0700

    Create InvokeSQLCmd.ps1

---
+++ SQL/InvokeSQLCmd.ps1
@@ -0,0 +1,4 @@
+﻿$SQLDBName = "AdventureWorks"
+$Table = 'HumanResources.Employee'
+$SqlQuery = "select * FROM $Table"
+Invoke-Sqlcmd -Query $SqlQuery -ServerInstance sql2 -Database $SQLDBName | Out-GridView
