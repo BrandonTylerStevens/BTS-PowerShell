d634bfe3ef2700ed5d7a07510d58fcb00bad1b39
Brandon Stevens <brandontylerstevens@gmail.com>
2018-01-21 07:11:25 -0700

    Create SimpleTry.ps1

---
+++ Error Handling/SimpleTry.ps1
@@ -0,0 +1,27 @@
+﻿try
+{
+    "trying"
+    Get-Process -ErrorAction stop -ErrorVariable myerr -Name notepads
+    Get-Service netlogon
+
+
+}
+catch [Microsoft.PowerShell.Commands.ProcessCommandException]
+{
+    $_.exception.ProcessName + "Does not exist"
+    $procName = Read-Host "Enter the correct Process Name"
+    Get-Process -Name $procname
+    #$_ | Format-List * -Force
+}
+catch
+{
+
+    $_ | Format-List * -Force
+}
+Finally
+{
+
+ "clean up"
+}
+
+"next command"
