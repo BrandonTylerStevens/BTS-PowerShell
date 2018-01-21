5b3be1a76f21db9211299f8ed80fb59c0f8c2227
Brandon Stevens <brandontylerstevens@gmail.com>
2018-01-21 07:15:27 -0700

    Create SimplePasswordEncryption.ps1

---
+++ Encryption/SimplePasswordEncryption.ps1
@@ -0,0 +1,3 @@
+﻿"Password1" | ConvertTo-SecureString -Force -AsPlainText | ConvertFrom-SecureString  > .\password.txt
+$password = Get-Content .\password.txt | ConvertTo-SecureString
+New-Object -TypeName PSCredential -ArgumentList "Contoso\UserName",$password
