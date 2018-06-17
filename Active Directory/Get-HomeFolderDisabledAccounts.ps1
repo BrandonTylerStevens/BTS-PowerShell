5b3506a1a1478762f72174a9094de3ac2162d7b0
Brandon Stevens <brandontylerstevens@gmail.com>
2018-06-17 08:28:50 -0700

    Add files via upload

---
+++ Active Directory/Get-HomeFolderDisabledAccounts.ps1
@@ -0,0 +1,27 @@
+﻿Function Get-HomeFolderDisabledAccounts {
+Import-Module ActiveDirectory
+$logfile = "C:\Temp\HomeFolderDisabledAccounts.csv"
+$input = read-host "Enter path Example \\server\share"
+
+If ((Test-Path $input) -eq $true) {
+		$folders = Get-ChildItem $input
+
+		$count = 0
+
+		Add-Content $logfile "samaccountname,enabled,fullpath,description"
+
+		$folders | ForEach-Object{
+        $account = "account not found"
+		$folder = $_
+		$count = $count + 1
+		$account = Get-aduser -identity  $folder.name -properties Description
+		Add-Content $logfile "$($account.samaccountname),$($account.enabled),$($folder.fullname),$($account.description)"
+		Write-Host "Working on $count of $($folders.count)"
+		}
+
+
+}
+else {
+		Write-Host "Invalid path"
+}
+}
