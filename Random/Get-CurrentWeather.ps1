8b56ba655b73ec23a79779f4920d9e872b2fea13
Brandon Stevens <brandontylerstevens@gmail.com>
2018-06-17 08:53:06 -0700

    Add files via upload

---
+++ Random/Get-CurrentWeather.ps1
@@ -0,0 +1,28 @@
+﻿Function Get-CurrentWeather {
+<#
+.SYNOPSIS
+    Just a simple PowerShell script to automate the retrieval your weather via API key.
+.DESCRIPTION
+    Your API key at openweathermap.org is fe5c2a60e7255b20b113c52187629653
+.INSTRUCTIONS
+    The free api key only permits querying once every 10 minutes...
+    Can download city ID codes here: http://bulk.openweathermap.org/sample/
+    To convert Kelvin to Fahrenheit:
+    TempKelvin * 9/5 - 459.67
+.NOTES
+    File Name      : Get-CurrentWeather.ps1
+    Author         : Brandon Stevens (BrandonTylerStevens@gmail.com)
+    Prerequisite   : PowerShell V2
+    Copyright 2018 - Brandon Stevens
+.LINK
+    http://bulk.openweathermap.org/sample/
+.EXAMPLE
+     
+#>
+
+$GilbertCityID = 5027943
+$apiKey = '4023d5224a761208c662e83cd471161a'
+#$uri = "api.openweathermap.org/data/2.5/weather?q=Denver,us&mode=json&APPID=$apiKey"
+$uri = "api.openweathermap.org/data/2.5/weather?id=$GilbertCityID&mode=json&APPID=$apiKey"
+$results = Invoke-RestMethod $uri
+}
---
+++ Random/Get-InitialDirectoryOfFile.ps1
@@ -0,0 +1,12 @@
+﻿#using assembly "System.windows.forms"
+Function Get-FileName
+{ 
+param($initialDirectory = "c:\")
+
+#add-type  -AssemblyName "System.windows.forms"
+ $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+ $OpenFileDialog.initialDirectory = $initialDirectory
+ $OpenFileDialog.filter = "All files (*.*)| *.*"
+ $OpenFileDialog.ShowDialog() | Out-Null
+ $OpenFileDialog.filename
+} #end function Get-FileName
