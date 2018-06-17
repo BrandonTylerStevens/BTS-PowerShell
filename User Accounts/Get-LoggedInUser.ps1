f1096d8519f5d430654666864d54d2341067de05
Brandon Stevens <brandontylerstevens@gmail.com>
2018-06-17 08:56:08 -0700

    Add files via upload

---
+++ User Accounts/Get-LoggedInUser.ps1
@@ -0,0 +1,17 @@
+﻿# -------------------------------------------
+# Function Name: Get-LoggedIn
+# Return the current logged-in user of a remote machine.
+# -------------------------------------------
+Function Get-LoggedInUser {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$True)]
+    [string[]]$computername
+  )
+
+  foreach ($pc in $computername){
+    $logged_in = (gwmi win32_computersystem -COMPUTER $pc).username
+    $name = $logged_in.split("\")[1]
+    "{0}: {1}" -f $pc,$name
+  }
+}
