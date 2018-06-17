0770ce06157e7b8aa872547c46a75f73c02fbc3a
Brandon Stevens <brandontylerstevens@gmail.com>
2018-06-17 08:55:19 -0700

    Add files via upload

---
+++ SQL/Invoke-SQLCmd.ps1
@@ -0,0 +1,5 @@
+﻿Function Invoke-SQLCmd {$SQLDBName = "AdventureWorks"
+$Table = 'HumanResources.Employee'
+$SqlQuery = "select * FROM $Table"
+Invoke-Sqlcmd -Query $SqlQuery -ServerInstance sql2 -Database $SQLDBName | Out-GridView
+}
